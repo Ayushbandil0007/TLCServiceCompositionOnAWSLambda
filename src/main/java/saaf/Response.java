ddc45aa79d83bb55ca81aa8c409caa519f8cfa11
Ayush Bandil <ayushbandil@finmechanics.com>
2019-12-12 12:05:13 -0800

    Sanitization

--- src/main/java/saaf/Response.java
+++ src/main/java/saaf/Response.java
@@ -1,12 +1,10 @@
 package saaf;
 
+
 /**
- * A basic Response object that can be consumed by FaaS Inspector
- * to be used as additional output.
- * 
- * @author Wes Lloyd
- * @author Robert Cordingly
+ * Created by Ayush Bandil on 12/12/2019.
  */
+
 public class Response {
     //
     // User Defined Attributes
